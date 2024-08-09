144f8e7c482ec47eab25d384e89e725062317bf7
rogulik <malvla2008@gmail.com>
2024-08-09 11:55:30 +0300

    ApplicationUser and PublisherSubscriber are added

---
+++ EichkustMusic.Users.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace EichkustMusic.Users.Domain.Entities
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+        [Url]
+        public string? PictureUrl {  get; set; }
+
+        [MaxLength(1024)]
+        public string? Desciption { get; set; }
+
+        [Required]
+        [MaxLength(64)]
+        public string DisplayName { get; set; } = null!;
+
+        public IEnumerable<PublisherSubscriber> PublisherSubscribers { get; set; }
+            = new List<PublisherSubscriber>();
+
+        public IEnumerable<ApplicationUser> Subscriptions
+        {
+            get
+            {
+                return PublisherSubscribers.Select(publisherSubscriber => publisherSubscriber.Publisher);
+            }
+        }
+    }
+}
---
+++ EichkustMusic.Users.Domain/Entities/PublisherSubscriber.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EichkustMusic.Users.Domain.Entities
+{
+    public class PublisherSubscriber
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public int PublisherId { get; set; }
+
+        [Required]
+        public int SubscriberId { get; set; }
+
+        [ForeignKey(nameof(PublisherId))]
+        public ApplicationUser Publisher { get; set; } = null!;
+
+        [ForeignKey(nameof(SubscriberId))]
+        public ApplicationUser Subscriber { get; set; } = null!;
+    }
+}
