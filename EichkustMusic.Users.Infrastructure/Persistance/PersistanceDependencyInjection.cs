7ee8c3d147a978d6bce95ee625f43bb538541538
rogulik <malvla2008@gmail.com>
2024-08-10 11:53:15 +0300

    Database context dependency injection files added

---
+++ EichkustMusic.Users.Infrastructure/Persistance/PersistanceDependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EichkustMusic.Users.Infrastructure.Persistance
+{
+    public static class PersistanceDependencyInjection
+    {
+        public static IServiceCollection AddPersistace(
+            this IServiceCollection services, IConfigurationManager configuration)
+        {
+            services.AddDbContext<UsersDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("UsersDb")));
+
+            return services;
+        }
+    }
+}
