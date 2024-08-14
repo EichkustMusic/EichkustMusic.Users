f8f35f8be22af661524ae9a9a253e646e2e58d97
rogulik <malvla2008@gmail.com>
2024-08-14 11:21:06 +0300

    User repository inteface written

--- EichkustMusic.Users.API/obj/Debug/net8.0/EichkustMusic.Users.API.AssemblyInfo.cs
+++ EichkustMusic.Users.API/obj/Debug/net8.0/EichkustMusic.Users.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EichkustMusic.Users.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a33a9dbafe309e513870a2c4683bd7e037e71faf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cc1ef2e95b8f2e52f6e7f8470dafb21418fd026")]
 [assembly: System.Reflection.AssemblyProductAttribute("EichkustMusic.Users.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EichkustMusic.Users.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ EichkustMusic.Users.Application/Persistance/IUserRepository.cs
@@ -0,0 +1,28 @@
+﻿using EichkustMusic.Users.Domain.Entities;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace EichkustMusic.Users.Application.UserRepository
+{
+    public interface IUserRepository
+    {
+        Task<ApplicationUser?> GetUserByIdAsync(int id);
+
+        Task<ApplicationUser> RegisterUserAsync(ApplicationUser user, string password);
+
+        // Description must be shortened!
+        Task<ICollection<ApplicationUser>> ListUsersAsync(
+            int pageNum, int pageSize, string? query);  
+
+        // Document must be checked if it contains paths to S3 files. If it contains, patch should be rejected
+        Task ApplyPatchDocumentAsyncTo(ApplicationUser user, JsonPatchDocument patchDocument);
+
+        // Files must be deleted from S3
+        Task DeleteUser(ApplicationUser user);  
+
+        void AddSubscription(ApplicationUser subscriber, ApplicationUser publisher);
+
+        void DeleteSubscription(ApplicationUser subscriber, ApplicationUser publisher);
+
+        Task SaveChangesAsync();
+    }
+}
