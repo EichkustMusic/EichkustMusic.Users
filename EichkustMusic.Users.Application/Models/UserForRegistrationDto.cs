cdb996334fd4411f74b58158ce0aa7a3bb17bc32
rogulik <malvla2008@gmail.com>
2024-08-19 11:31:00 +0300

    User registration DTO written

---
+++ EichkustMusic.Users.Application/Models/UserForRegistrationDto.cs
@@ -0,0 +1,37 @@
+﻿using EichkustMusic.Users.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace EichkustMusic.Users.Application.Models
+{
+    public class UserForRegistrationDto
+    {
+        [Required]
+        [MaxLength(32)]
+        public string UserName { get; set; } = null!;
+
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Phone]
+        public string? PhoneNumber { get; set; }
+
+        [MaxLength(1024)]
+        public string? Description { get; set; }
+
+        [Required]
+        [MaxLength(64)]
+        public string DisplayName { get; set; } = null!;
+
+        public ApplicationUser MapToApplicationUser()
+        {
+            return new ApplicationUser()
+            {
+                UserName = UserName,
+                Email = Email,
+                PhoneNumber = PhoneNumber,
+                Desciption = Description,
+                DisplayName = DisplayName,
+            };
+        }
+    }
+}
